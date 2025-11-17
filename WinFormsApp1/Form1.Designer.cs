871be31c6f02890f98d9317cdbcdc6a35f716189
Eren Vural <erenvural49@gmail.com>
2025-11-17 14:43:33 +0300

    NOTE APP

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveAsToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            designToolStripMenuItem = new ToolStripMenuItem();
+            fontToolStripMenuItem = new ToolStripMenuItem();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            colorDialog1 = new ColorDialog();
+            fontDialog1 = new FontDialog();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            menuStrip1.SuspendLayout();
+            tabControl1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, designToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 28);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveAsToolStripMenuItem, saveToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(46, 24);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(141, 26);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            saveAsToolStripMenuItem.Size = new Size(141, 26);
+            saveAsToolStripMenuItem.Text = "Save as";
+            saveAsToolStripMenuItem.Click += saveAsToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(141, 26);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // designToolStripMenuItem
+            // 
+            designToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { fontToolStripMenuItem, colorToolStripMenuItem });
+            designToolStripMenuItem.Name = "designToolStripMenuItem";
+            designToolStripMenuItem.Size = new Size(69, 24);
+            designToolStripMenuItem.Text = "Design";
+            // 
+            // fontToolStripMenuItem
+            // 
+            fontToolStripMenuItem.Name = "fontToolStripMenuItem";
+            fontToolStripMenuItem.Size = new Size(128, 26);
+            fontToolStripMenuItem.Text = "Font";
+            fontToolStripMenuItem.Click += fontToolStripMenuItem_Click;
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(128, 26);
+            colorToolStripMenuItem.Text = "Color";
+            colorToolStripMenuItem.Click += colorToolStripMenuItem_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 31);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(280, 146);
+            tabControl1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Location = new Point(4, 29);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(272, 113);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            tabPage1.Click += tabPage1_Click;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 29);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(272, 113);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 128, 0);
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            tabControl1.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveAsToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem designToolStripMenuItem;
+        private ToolStripMenuItem fontToolStripMenuItem;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private OpenFileDialog openFileDialog1;
+        private SaveFileDialog saveFileDialog1;
+        private ColorDialog colorDialog1;
+        private FontDialog fontDialog1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,180 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath = null!;
+        private RichTextBox richTextBox1 = null!;
+        private RichTextBox richTextBox2 = null!;
+        private bool isDirty = false;
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+            //How form understand is there any changes in the text?
+            //windows forms detects user input,richtexbox updates its text,richtexbox automatically calls TextChanged event.
+            //
+            //"When richTextBox1's text changes, call my richTextBox_TextChanged method"
+            richTextBox1.TextChanged += richTextBox_TextChanged;
+            richTextBox2.TextChanged += RichTextBox_TextChanged;
+            
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string filePath = openFileDialog1.FileName;
+                string extension = Path.GetExtension(filePath);
+
+                if (extension.ToLower() == ".txt")
+                {
+                    richTextBox1.LoadFile(filePath, RichTextBoxStreamType.PlainText);
+                }
+                else if (extension.ToLower() == ".rtf")
+                {
+                    richTextBox1.LoadFile(filePath, RichTextBoxStreamType.RichText);
+                }
+                currentFilePath = filePath;
+
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //You should show dialog both .txt and .rtf files.
+            DialogResult result = saveFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                string filePath = saveFileDialog1.FileName;
+                string extension = Path.GetExtension(filePath);
+
+                if (extension.ToLower() == ".txt")
+                {
+                    richTextBox1.SaveFile(filePath, RichTextBoxStreamType.PlainText);
+                }
+
+                else if (extension.ToLower() == ".rtf")
+                {
+                    richTextBox1.SaveFile(filePath, RichTextBoxStreamType.RichText);
+                }
+                currentFilePath = filePath;
+
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string extension = string.IsNullOrEmpty(currentFilePath) 
+            ? "" : Path.GetExtension(currentFilePath);
+
+            if (extension.ToLower() == "")
+            {
+                saveAsToolStripMenuItem_Click(sender, e);
+
+            }
+
+            if (extension.ToLower() == ".txt")
+                richTextBox1.SaveFile(currentFilePath, RichTextBoxStreamType.PlainText);
+            else if (extension.ToLower() == ".rtf")
+                richTextBox1.SaveFile(currentFilePath, RichTextBoxStreamType.RichText);
+
+        }
+
+        private void colorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            string extension = string.IsNullOrEmpty(currentFilePath) 
+            ? "" : Path.GetExtension(currentFilePath);
+
+            if (extension.ToLower() == ".rtf")
+            {
+                //You should only show dialog .rtf files.
+                DialogResult result = colorDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                    richTextBox1.SelectionColor = colorDialog1.Color;
+
+            }
+        }
+        private void fontToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string extension = string.IsNullOrEmpty(currentFilePath) 
+            ? "" : Path.GetExtension(currentFilePath);
+
+            if (extension.ToLower() == ".rtf")
+            {
+                DialogResult result = fontDialog1.ShowDialog();
+                if (result == DialogResult.OK)
+                    richTextBox1.SelectionFont = fontDialog1.Font;
+            }
+            else
+            {
+                MessageBox.Show("You didn't open any file or you are in a text file.");
+
+            }
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TabControl full page
+            tabControl1.Dock = DockStyle.Fill;
+
+            // Create richTextBox1
+            richTextBox1 = new RichTextBox();
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Name = "richTextBox1";
+            //add RichTextBox1 to tabPage1
+            tabPage1.Controls.Add(richTextBox1);
+
+            // Create richTextBox2
+            richTextBox2 = new RichTextBox();
+            richTextBox2.Dock = DockStyle.Fill;
+            richTextBox2.Name = "richTextBox2";
+            // and add RichTextBox2 to tabPage2
+            tabPage2.Controls.Add(richTextBox2);
+        }
+        private void richTextBox_TextChanged(object sender, EventArgs e){
+            isDirty = true;
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e){
+    
+    if (isDirty == true)
+    {
+        
+        MessageBox.Show("You have unsaved changes. Do you want to save before closing?", "Unsaved Changes", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+        DialogResult result = MessageBox.Show(
+            "You have unsaved changes. Do you want to save before closing?",
+            "Unsaved Changes",
+            MessageBoxButtons.YesNoCancel,
+            MessageBoxIcon.Question
+        );
+        
+        if (result == DialogResult.Yes)
+        {
+            saveToolStripMenuItem_Click(sender,e);
+            isDirty= false;
+
+        }
+        else if (result == DialogResult.Cancel)
+        {
+            //dont close the form
+            e.Cancel = true;
+        }
+        else if (result == DialogResult.No)
+        {
+            e.Cancel = false;
+            //close the form
+            //default olarak da false zaten.
+        }
+    }
+}
+    }
+}
